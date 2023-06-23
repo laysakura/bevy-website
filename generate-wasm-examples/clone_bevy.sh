9a7a1cd81f6698b856d6f77c44c0d526457d1c3e
Sho Nakatani <lay.sakura@gmail.com>
2023-06-24 06:43:25 +0900

    laysakura/bevyを使う

--- generate-wasm-examples/clone_bevy.sh
+++ generate-wasm-examples/clone_bevy.sh
@@ -3,6 +3,5 @@
 rm -rf bevy
 git init bevy
 cd bevy
-# git remote add origin https://github.com/laysakura/bevy
-git remote add origin https://github.com/bevyengine/bevy
+git remote add origin https://github.com/laysakura/bevy
 git pull --depth=1 origin main
